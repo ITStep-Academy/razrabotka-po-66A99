71587d518caa858338954dbfac90b04aa75ee601
66A99 <69242629+66A99@users.noreply.github.com>
2020-08-05 17:04:39 +0500

    Update Source.c

--- Source.c
+++ Source.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
 void main() {
-	/*double a;
-	int b;
-	double c;
-	printf("input a fractional number:");
-	scanf_s("%lf", &a);
-	b = (int) a;
-	c = (double) a - b;
-	printf("%.2lf", c);*/
+	
 	int a;
 	int b;
 	scanf_s("%d", &a);
